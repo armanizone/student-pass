c34ac49f3aaa63b734f47fd3ccf63fc1b1c1888f
armaizone <kurama.zxc@mail.ru>
2025-10-30 16:06:24 +0500

    w

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import student from './assets/file-1.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black'>
+      <img src={student} alt="react logo" className='mx-auto w-screen h-screen object-contain' />
+    </div>
+  )
+}
+
+export default App
